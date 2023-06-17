d9afc84fefe5cd0c4f8d86d52f5783fe473090d6
Aadith <2004aadith@gmail.com>
2023-06-17 17:48:58 +0530

    found out an issue in updating the wallet, with the encryption of balance fixed that

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -101,8 +101,7 @@ export class AuthService {
   async passWordReset(email: string) {
     const user = await this.userService.getUserByEmail(email);
 
-    if (!user || !user.is_active || user.is_deleted)
-      throw new UnauthorizedException('Email does not exist');
+    if (!user || !user.is_active || user.is_deleted) throw new UnauthorizedException('Email does not exist');
     const otp = this.generateOTP();
     const setOtp = await this.userService.updateOtp(email, otp);
     this.resetJob = schedule.scheduleJob(
--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -5,25 +5,25 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class WalletService {
-  private readonly encryptionKey = process.env.WALLET_ENCRYPTION_KEY; 
+  private readonly encryptionKey = process.env.WALLET_ENCRYPTION_KEY;
   constructor(
     @InjectModel(Wallet.name) private readonly walletModel: Model<Wallet>,
   ) {}
 
   async getWalletBalance(user) {
     try {
-      const wallet:any = await this.walletModel.findOne({ user_id: user.sub });
-      if (!wallet){
-        const newWallet:any =  await this.walletModel.create({
+      const wallet: any = await this.walletModel.findOne({ user_id: user.sub });
+      if (!wallet) {
+        const newWallet: any = await this.walletModel.create({
           user_id: user.sub,
           currency: 'aed',
-          balance:this.encryptBalance(0)
-        })
-         newWallet.balance = this.decryptBalance(newWallet.balance);
-         return newWallet;
+          balance: this.encryptBalance(0),
+        });
+        newWallet.balance = this.decryptBalance(newWallet.balance);
+        return newWallet;
       }
-       wallet.balance = this.decryptBalance(wallet.balance);
-       return wallet;
+      wallet.balance = this.decryptBalance(wallet.balance);
+      return wallet;
     } catch (error) {
       return error;
     }
@@ -31,31 +31,37 @@ export class WalletService {
 
   async updateUserWalletBalance(userId: string, amount: number) {
     try {
-      const wallet = await this.walletModel.findOne({ user_id: userId });     
-      const decryptedBalabce = this.decryptBalance(wallet.balance)
-      const newbalance = this.encryptBalance(amount+decryptedBalabce)
+      const wallet = await this.walletModel.findOne({ user_id: userId });
+
       if (wallet) {
+        const decryptedBalabce = this.decryptBalance(wallet.balance);
+        const newbalance = this.encryptBalance(amount + decryptedBalabce);
         this.walletModel
           .updateOne({ user_id: userId }, { $set: { balance: newbalance } })
           .then((data) => {
             console.log(data);
           });
       } else {
-         this.walletModel.create({ user_id: userId, balance: newbalance,currency:'aed' }).then((data)=>{
-          console.log(data);
-         }).catch((err)=>console.log(err)
-         )
+        const newbalance = this.encryptBalance(amount);
+
+        this.walletModel
+          .create({ user_id: userId, balance: newbalance, currency: 'aed' })
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((err) => console.log(err));
       }
     } catch (error) {
       return error;
     }
   }
+
   private encryptBalance(balance: number): string {
     const key = Buffer.from(this.encryptionKey, 'utf8');
     const buffer = Buffer.allocUnsafe(8);
     buffer.writeInt32BE(balance, 0);
     for (let i = 0; i < buffer.length; i++) {
-      buffer[i] = buffer[i] ^ key[i % key.length];
+      buffer[i] ^= key[i % key.length];
     }
     return buffer.toString('base64');
   }
@@ -64,7 +70,7 @@ export class WalletService {
     const key = Buffer.from(this.encryptionKey, 'utf8');
     const buffer = Buffer.from(encryptedBalance, 'base64');
     for (let i = 0; i < buffer.length; i++) {
-      buffer[i] = buffer[i] ^ key[i % key.length];
+      buffer[i] ^= key[i % key.length];
     }
     return buffer.readInt32BE(0);
   }
